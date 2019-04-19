0ad68b0c3dcc619218773a45a8c4e51884f02248
nicncs <nic@kaodim.com>
2019-04-19 16:24:50 +0800

    Clean

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import Main from "./Main"
-import React, { Component } from "react"
-import ReactDOM from "react-dom"
-
-import './css/main.css'
-
-const rootElem = document.getElementById("root")
-rootElem?ReactDOM.render(<Main />, rootElem) : false
--- src/utils/screen.js
+++
@@ -1,9 +0,0 @@
-const screenSize = () => {
-    let width = window.outerWidth
-    if (width>=992) return 'lg'
-    if (width>=768) return 'md'
-    if (width>=576) return 'sm'
-    if (width<576) return 'xs'
-}
-
-export { screenSize }
